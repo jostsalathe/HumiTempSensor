fb099c54ac16e2b2bb8d04e2cf097d51dfc2e048
jostsalathe <jostsalathe@gmail.com>
2020-04-11 04:36:15 +0200

    Implemented use of SPIFFS for configuration files. Now the firmware doesn't need to be recompiled just for changing parameters.

--- HumiTempSensor.ino
+++ HumiTempSensor.ino
@@ -5,15 +5,17 @@
 #include <FS.h>
 #include <ThingsBoard.h>
 
-#define WIFI_SSID "OmasVonDerMuellhalde"
-#define WIFI_PASSWORD "Dieser Film macht betroffen."
-IPAddress staticIP(192,168,0,70);
-IPAddress gateway(192,168,0,1);
-IPAddress subnet(255,255,255,0);
+String wifiSsid;
+String wifiPassword;
+IPAddress staticIP;
+IPAddress gateway;
+IPAddress subnet;
 
-#define TOKEN "7HjaOhoGK4nnnmwL8BGW"
+String thingsboardServer;
+String token;
 
-#define SLEEPMS 1000 * 30
+long intervalMs;
+
 
 #define DEBUG_EN 14
 #define LED_ST_0 4
@@ -25,8 +27,6 @@ IPAddress subnet(255,255,255,0);
 
 #define SDA 2
 #define SCL 0
-
-char thingsboardServer[] = "192.168.0.10";
 
 WiFiClient wifiClient;
 
@@ -54,47 +54,115 @@ unsigned long lastSend;
 void setup()
 {
   pinMode(DEBUG_EN, INPUT_PULLUP);
-  if (digitalRead(DEBUG_EN))
-  {
-    // internally pulled high - solder bridge open
-    debug = false;
-  }
-  else
-  {
-    // externally pulled low - solder bridge closed
-    debug = true;
-  }
+  // internally pulled high - solder bridge open
+  if (digitalRead(DEBUG_EN)) debug = false;
+  else debug = true;
   pinMode(LED_ST_0, OUTPUT);
   pinMode(LED_ST_1, OUTPUT);
   digitalWrite(LED_ST_0, LOW);
   digitalWrite(LED_ST_1, LOW);
   
-  Wire.begin(SDA, SCL);
   Serial.begin(74880);
-  bme.begin();
-  
-  startCconnectWiFi();  
-  
-  getAndReportSensorData();
-  
-  unsigned int sleepMs = (static_cast<int64_t>(SLEEPMS) - millis() < 0) ? 1 : SLEEPMS - millis();
-  if (debug)
-  {
-    Serial.println("Entering deep sleep for " + String(sleepMs) + "ms...");
-  }
-  ESP.deepSleep(static_cast<uint64_t>(sleepMs) * 1000);
+  Wire.begin(SDA, SCL);
+
+  if (readConfig()) getAndReportSensorDataThenSleep();
+  else Serial.println("handling faulty configuration");
 }
 
 void loop()
 {
+  digitalWrite(LED_ST_1, HIGH);
+  delay(50);
+  digitalWrite(LED_ST_0, HIGH);
+  delay(200);
+  digitalWrite(LED_ST_1, LOW);
+  delay(50);
+  digitalWrite(LED_ST_0, LOW);
+  delay(200);
 }
 
-void getAndReportSensorData()
+bool readConfig()
 {
-  if (debug)
+  if (debug) Serial.println("reading configuration files from SPIFFS...");
+
+  // do stuff
+  SPIFFS.begin();
+
+  bool success = true;
+  if (!readFirstLineFromFile(wifiSsid, "/ssid.txt"))            success = false;
+  if (!readFirstLineFromFile(wifiPassword, "/pwd.txt"))         success = false;
+  if (!readFirstIpFromFile(staticIP, "/ip.txt"))                success = false;
+  if (!readFirstIpFromFile(gateway, "/gateway.txt"))            success = false;
+  if (!readFirstIpFromFile(subnet, "/subnet.txt"))              success = false;
+
+  if (!readFirstLineFromFile(thingsboardServer, "/server.txt")) success = false;
+  if (!readFirstLineFromFile(token, "/token.txt"))              success = false;
+
+  if (!readFirstPositiveIntegerFromFile(intervalMs, "/intervalMs.txt"))    success = false;
+  if (intervalMs < 1)
   {
-    Serial.println("Acquire new sensor data...");
+    Serial.println("ERROR: measurement interval is " + String(intervalMs) + " but must be >0");
+    success = false;
   }
+  
+  if (debug) Serial.println("reading configuration files from SPIFFS - done.");
+  return success;
+}
+
+bool openFile(File & f, String fileName)
+{
+  if (debug) Serial.println("reading file \"" + fileName + "\"");
+  f = SPIFFS.open(fileName, "r");
+  if (!f) {
+    Serial.println("ERROR: couldn't open file \"" + fileName + "\"");
+    f.close();
+    return false;
+  }
+  return true;
+}
+
+bool readFirstLineFromFile(String & ret, String fileName)
+{
+  File f;
+  if (!openFile(f, fileName)) return false;
+  ret = f.readString();
+  ret = ret.substring(0, ret.indexOf('\r'));
+  ret = ret.substring(0, ret.indexOf('\n'));
+  f.close();
+  return true;
+}
+
+bool readFirstPositiveIntegerFromFile(long & ret, String fileName)
+{
+  String sNumber;
+  if (!readFirstLineFromFile(sNumber, fileName)) return false;
+  else if ((ret = sNumber.toInt()) <= 0)
+  {
+    Serial.println("ERROR: couldn't convert \"" + sNumber + "\" to positive integer");
+    return false;
+  }
+  return true;
+}
+
+bool readFirstIpFromFile(IPAddress & ret, String fileName)
+{
+  String sAddr;
+  if (!readFirstLineFromFile(sAddr, fileName)) return false;
+  else if (!ret.fromString(sAddr))
+  {
+    Serial.println("ERROR: couldn't convert \"" + sAddr + "\" to IP address");
+    return false;
+  }
+  return true;
+}
+
+void getAndReportSensorDataThenSleep()
+{
+  startCconnectWiFi();  
+  
+  bme.begin();
+  
+  if (debug) Serial.println("Acquire new sensor data...");
   // pressure in hPa
   float pressure = NAN;
   // temperature in Celsius
@@ -106,13 +174,15 @@ void getAndReportSensorData()
   bme.read(pressure, temperature, humidity, BME280I2C::TempUnit_Celsius, BME280I2C::PresUnit_hPa);
   delay(1000);
   bme.read(pressure, temperature, humidity, BME280I2C::TempUnit_Celsius, BME280I2C::PresUnit_hPa);
-  if (debug)
-  {
-    Serial.println("Measured: pres=" + String(pressure,2) + "hPa, temp=" + String(temperature,2) + "°C, hum=" + String(humidity,2) + "%");
-  }
+  if (debug) Serial.println("Measured: pres=" + String(pressure,2) + "hPa, temp=" + String(temperature,2) + "°C, hum=" + String(humidity,2) + "%");
 
   displayMeasurements(humidity, temperature, pressure);
   reportMeasurements(humidity, temperature, pressure);
+  
+  long sleepMs = intervalMs - millis();
+  if (sleepMs < 1) sleepMs = 1;
+  if (debug) Serial.println("Entering deep sleep for " + String(sleepMs) + "ms...");
+  ESP.deepSleep(static_cast<uint64_t>(sleepMs) * 1000);
 }
 
 void displayMeasurements(float hum, float temp, float pres)
@@ -140,17 +210,11 @@ void displayMeasurements(float hum, float temp, float pres)
 
 void reportMeasurements(float hum, float temp, float pres)
 {
-  if (debug)
-  {
-    Serial.println("Transmit to server...");
-  }
+  if (debug) Serial.println("Transmit to server...");
   
   if (isnan(hum) || isnan(temp) || isnan(pres))
   {
-    if (debug)
-    {
-      Serial.println("Sensor read failed, faulty data not transmitted!");
-    }
+    if (debug) Serial.println("ERROR: Sensor read failed, faulty data not transmitted!");
     return;
   }
 
@@ -164,12 +228,9 @@ void reportMeasurements(float hum, float temp, float pres)
     Serial.println("Connecting to ThingsBoard...");
     digitalWrite(LED_ST_1, HIGH);
   }
-  if (tb.connect(thingsboardServer, TOKEN))
+  if (tb.connect(thingsboardServer.c_str(), token.c_str()))
   {
-    if (debug)
-    {
-      Serial.println("Sending data to ThingsBoard...");
-    }
+    if (debug) Serial.println("Sending data to ThingsBoard...");
     // sending data to ThingsBoard
     const int data_items = 3;
     Telemetry data[data_items] =
@@ -187,20 +248,14 @@ void reportMeasurements(float hum, float temp, float pres)
   }
 
   // disconnecting from ThingsBoard
-  if (debug)
-  {
-    Serial.println("Disconnecting from ThingsBoard...");
-  }
+  if (debug) Serial.println("Disconnecting from ThingsBoard...");
   tb.disconnect();
   digitalWrite(LED_ST_1, LOW);
   
   // disconnecting from WiFi
   disconnectWiFi();
   
-  if (debug)
-  {
-    Serial.println("Transmit to server - done.");
-  }
+  if (debug) Serial.println("Transmit to server - done.");
 }
 
 bool startCconnectWiFi()
@@ -215,19 +270,13 @@ bool startCconnectWiFi()
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
   WiFi.config(staticIP, gateway, subnet);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(wifiSsid, wifiPassword);
 }
 
 bool waitConnectWiFi()
 {
-  if (debug)
-  {
-    Serial.println("Wait for WiFi...");
-  }
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(10);
-  }
+  if (debug) Serial.println("Wait for WiFi...");
+  while (WiFi.status() != WL_CONNECTED) delay(10);
   if (debug)
   {
     Serial.setDebugOutput(false);
@@ -237,10 +286,7 @@ bool waitConnectWiFi()
 
 void disconnectWiFi()
 {
-  if (debug)
-  {
-    Serial.println("Disconnecting from WiFi.");
-  }
+  if (debug) Serial.println("Disconnecting from WiFi.");
   WiFi.disconnect(true);
   delay(1);
   digitalWrite(LED_ST_0, LOW);
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # HumiTempSensor
 Firmware for little board around an ESP-12F using a BME280 to record temperature and rel. humidity to ThingsBoard.
+
+## What you need
+You need the following software components:
+- Arduino IDE
+- [esp8266 board package](https://github.com/esp8266/Arduino)
+- [ThingsBoard Arduino MQTT SDK](https://github.com/thingsboard/ThingsBoard-Arduino-MQTT-SDK)
+- [ThingPulse esp8266 ssd1306 oled library](https://github.com/ThingPulse/esp8266-oled-ssd1306)
+- [BME280 library by Tyler Glenn](https://github.com/finitespace/BME280)
+- [Arduino ESP8266 filesystem uploader plugin](https://github.com/esp8266/arduino-esp8266fs-plugin/)
+- [ThingsBoard server](https://thingsboard.io/) where the sensor can post its data and where you can view that data on dashboards
+- A token for the sensor that is registered in your ThingsBoard
+
+You need the following hardware components:
+- ESP12 board or compatible including USB programming adapter (e.g. NodeMCU)
+- BME280 sensor
+- SSD1306 128*32 OLED display module
+- power source
+- A WiFi network that the sensor can connect to
+
+I built a nice little board ([ESP8266 12F BME280](https://easyeda.com/jostsalathe/ESP8266-12e-Base-Board_copy)) that has everything except the programmer on board. For uploading data to this board I use a USB FTDI232 adapter.
+
+I recommend using some wire to mount the sensor with a few centimeters distance to the main board since the ESP gets a little warm even when only sampling every 30 second.
+
+## How to use it
+- Clone this repository
+- Go to the data folder and modify all the files to your requirements
+- Upload the sketch via the *Arduino* IDE
+- Upload the filesystem data via *Arduino* IDE: *Tools > ESP8266 Sketch Data Upload*
